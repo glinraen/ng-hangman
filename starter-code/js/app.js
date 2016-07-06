9f1185a765a52c837ec3b8225b3fc22d0c101250
GLINRAEN <MISTRESS.WYN@GMAIL.COM>
2016-07-06 12:06:11 -0700

    adds game logic

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -1,4 +1,22 @@
 console.log('app.js loaded!');
 
 // initialize the application
-var app = angular.module("hangmanApp", []);
+angular
+  .module("hangmanApp", [])
+  .controller("hangmanCtrl", hangmanCtrl);
+
+
+  // logic goes here
+  
+
+  hangmanCtrl.$inject = ['$scope'];
+  function hangmanCtrl($scope){
+    $scope.hangman = new HangmanGame('elephant');
+    $scope.guessLetter = function (input){
+      $scope.hangman.guess(input);
+      $scope.hangman.input = "";
+    };
+  }
+
+
+
